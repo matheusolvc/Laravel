b9079d73fb8e185bfd0724571759034ef7645eb8
Matheus <matheusolvc@gmail.com>
2019-12-03 14:41:19 -0300

    initial commit

---
+++ TG/contasaPagar/resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,63 @@
+@extends('welcome')
+@section('body-content')
+<div class="row">
+    <div class="col-sm-6 div-card">
+        <div class="card card-dashboard">
+            <div class="card-body">
+                <h5 class="card-title">Contas</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 div-card">
+        <div class="card card-dashboard">
+            <div class="card-body">
+                <h5 class="card-title">Reembolsos</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12 div-card">
+        <div class="card card-dashboard">
+            <div class="card-body">
+                <h5 class="card-title">Contas</h5>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
